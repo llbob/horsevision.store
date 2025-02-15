1409fc77ef0df12f65026be5a0e50042d81c1d18
llbob <miniporse@gmail.com>
2025-02-15 13:07:22 +0100

    Make header props optional..

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
-const Header = ({ text }: { text: string }) => {
+interface HeaderProps {
+  text?: string;
+}
+
+const Header = ({ text = "" }: HeaderProps) => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
