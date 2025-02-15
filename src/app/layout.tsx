c0bce7ec2d57ec9d15e33f867c236cfb7c4f671c
llbob <miniporse@gmail.com>
2025-02-15 16:00:18 +0100

    Fix favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     template: `%s | ${CMS_NAME}`,
     default: CMS_NAME,
   },
-  description: `A statically generated site using Next.js and ${CMS_NAME}.`,
+  description: `horse vision`,
 };
 
 export default function RootLayout({
@@ -50,17 +50,7 @@ export default function RootLayout({
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
         <meta name="theme-color" content="#000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
